05f3e582ee58a4b90ea45810a41dce4759e7773c
U_M1JYK <MZonov@alfabank.ru>
2025-02-12 13:17:13 +0100

    добавлен тест для проверки авторизации

--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 
+from clients.web.saucedemo_page import SauceDemoPage
 from clients.web.americor.main_page import MainPage
 from storage.urls import BaseUrls
 
@@ -20,6 +21,14 @@ class TestMainPage:
     ], indirect=True)
     def test_go_to_main_page(self, browser):
         with allure.step("Шаг: Перейти на главную страницу"):
-            MainPage().open(browser, BaseUrls.AMERICOR_UI_URL)
-        with allure.step("Проверка: Открылся каталог стратегий"):
-            assert BaseUrls.AMERICOR_UI_URL in MainPage().get_current_url(browser), "Открылась не та страница"
+            MainPage().open(browser, BaseUrls.SAUCEDEMO_UI_URL)
+        with allure.step("Шаг: Заполнить поле ЛОГИН"):
+            SauceDemoPage().fill_in_login_in_field(browser)
+        with allure.step("Шаг: Заполнить поле ПАРОЛЬ"):
+            SauceDemoPage().fill_in_password_in_field(browser)
+        with allure.step("Шаг: Нажатие на кнопку входа"):
+            SauceDemoPage().click_to_login_button(browser)
+        with allure.step("Проверка: Открылся каталог товаров"):
+            assert BaseUrls.SAUCEDEMO_INVENTORY_UI_URL in MainPage().get_current_url(browser), "Открылась не та страница"
+
+
